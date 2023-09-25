a114f15214ff5230d40b4fde0c021450c32bdee9
toboooo <60663629+toboooo@users.noreply.github.com>
2023-09-25 09:31:24 +0100

    Add files via upload

--- model_assessment.py
+++ model_assessment.py
@@ -22,19 +22,26 @@ def model_test(model, X_train, X_test, y_train, y_test, parameter_ranges, classi
 	if classification:
 		score = balanaced_accuracy_score(predictions, y_test)
 	else:
-                score = mean_absolute_error(predictions, y_test)
+		score = -mean_absolute_error(predictions, y_test)
 	return score, search_result.best_params_
 
 def select_model(X_train, X_test, y_train, y_test, classification=False):
 	if classification:
 		models = [RandomForestClassifier, SVC, MLPDropoutClassifier]
+		parameter_ranges_list = [{"n_estimators": [50, 100, 200], "max_depth": [5, 10, 20], "min_samples_leaf": [5, 20, 100], "max_features": [0.2, 0.5, 0.8]}, {"kernel": ["linear", "rbf", "sigmoid"], "C": [0.001, 0.1, 1.0, 10.0], "gamma": [0.001, 0.1, 1.0, 10.0]}, {"hidden_layer_sizes": [(256, 256), (512, 512), (1024, 1024), (2048, 2048), (256, 256, 256, 256), (512, 512, 512, 512)], "alpha": [0.0, 1e-6, 1e-5, 1e-4], "learning_rate": [0.01, 0.05, 0.1], "dropout_rate": [0.0, 0.2, 0.5]}]
 	else:
 		models = [RandomForestRegressor, SVR, MLPDropoutRegressor]
+		parameter_ranges_list = [{"n_estimators": [50, 100, 200], "max_depth": [5, 10, 20], "min_samples_leaf": [5, 20, 100], "max_features": [0.2, 0.5, 0.8]}, {"kernel": ["linear", "rbf", "sigmoid"], "C": [0.001, 0.1, 1.0, 10.0], "gamma": [0.001, 0.1, 1.0, 10.0], "epsilon": [0.001, 0.1, 1.0]}, {"hidden_layer_sizes": [(256, 256), (512, 512), (1024, 1024), (2048, 2048), (256, 256, 256, 256), (512, 512, 512, 512)], "alpha": [0.0, 1e-6, 1e-5, 1e-4], "learning_rate": [0.01, 0.05, 0.1], "dropout_rate": [0.0, 0.2, 0.5]}]
 	model_names = ["RF", "SVM", "DNN"]
-	parameter_ranges_list = [
-	best_score = 0
+	best_score = 0 if classification else -float("inf")
 	best_parameters = None
 	best_model = None
+	for i, model in enumerate(models):
+		test_score, parameters = model_test(model, X_train, X_test, y_train, y_test, parameter_ranges_list[i], classification=classification)
+		if test_score > best_score:
+			best_model = model_names[i]
+			best_parameters = parameters
+	return best_model, best_score, best_parameters
 
 def select_features(mols, y, parameter_ranges, classification=False):
 	descriptors = get_descriptors(mols)
@@ -51,3 +58,9 @@ def select_features(mols, y, parameter_ranges, classification=False):
 	for X, name in zip(descriptor_sets, descriptor_names):
 		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=False)
 		best_model, best_score, best_hyperparameters = select_model(X_train, X_test, y_train, y_test, classification=classification)
+		print(names)
+		print("Best model: %s" % best_model)
+		print("Test score: %f" % best_score)
+		print("Hyperparameters:")
+		print(best_parameters)
+		print("\n")
