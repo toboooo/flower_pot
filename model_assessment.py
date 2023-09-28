1676b7cf64ab1404cb80a12711f5f0ca002b179f
toboooo <60663629+toboooo@users.noreply.github.com>
2023-09-28 11:08:21 +0100

    Add files via upload

--- model_assessment.py
+++ model_assessment.py
@@ -16,8 +16,8 @@ def hyperparameter_search(model, X_train, y_train, parameter_ranges, classificat
 	grid_search.fit(X_train, y_train)
 	return grid_search
 
-def model_test(model, X_train, X_test, y_train, y_test, parameter_ranges, classification=False):
-	search_result = hyperparameter_search(model, X_train, y_train, parameter_ranges, classication=classification)
+def model_test(model, X_train, X_test, y_train, y_test, parameter_ranges, classification=False, n_jobs=1):
+	search_result = hyperparameter_search(model, X_train, y_train, parameter_ranges, classication=classification, n_jobs=n_jobs)
 	predictions = search_result.best_estimator_.predict(X_test)
 	if classification:
 		score = balanaced_accuracy_score(predictions, y_test)
@@ -25,7 +25,7 @@ def model_test(model, X_train, X_test, y_train, y_test, parameter_ranges, classi
 		score = -mean_absolute_error(predictions, y_test)
 	return score, search_result.best_params_
 
-def select_model(X_train, X_test, y_train, y_test, classification=False):
+def select_model(X_train, X_test, y_train, y_test, classification=False, n_jobs=1):
 	if classification:
 		models = [RandomForestClassifier, SVC, MLPDropoutClassifier]
 		parameter_ranges_list = [{"n_estimators": [50, 100, 200], "max_depth": [5, 10, 20], "min_samples_leaf": [5, 20, 100], "max_features": [0.2, 0.5, 0.8]}, {"kernel": ["linear", "rbf", "sigmoid"], "C": [0.001, 0.1, 1.0, 10.0], "gamma": [0.001, 0.1, 1.0, 10.0]}, {"hidden_layer_sizes": [(256, 256), (512, 512), (1024, 1024), (2048, 2048), (256, 256, 256, 256), (512, 512, 512, 512)], "alpha": [0.0, 1e-6, 1e-5, 1e-4], "learning_rate": [0.01, 0.05, 0.1], "dropout_rate": [None, 0.2, 0.5]}]
@@ -37,13 +37,13 @@ def select_model(X_train, X_test, y_train, y_test, classification=False):
 	best_parameters = None
 	best_model = None
 	for i, model in enumerate(models):
-		test_score, parameters = model_test(model, X_train, X_test, y_train, y_test, parameter_ranges_list[i], classification=classification)
+		test_score, parameters = model_test(model, X_train, X_test, y_train, y_test, parameter_ranges_list[i], classification=classification, n_jobs)
 		if test_score > best_score:
 			best_model = model_names[i]
 			best_parameters = parameters
 	return best_model, best_score, best_parameters
 
-def select_features(mols, y, parameter_ranges, classification=False):
+def select_features(mols, y, parameter_ranges, classification=False, n_jobs=1):
 	descriptors = get_descriptors(mols)
 	fingerprints = get_fingerprints(mols)
 	descriptors_3d = get_3d_descriptors(mols)
@@ -57,7 +57,7 @@ def select_features(mols, y, parameter_ranges, classification=False):
 	descriptor_names = ["descriptors", "fingerprints", "3d", "descriptors+fingerprints", "descriptors+3d", "fingerprints+3d", "descriptors+fingerprints+3d"]
 	for X, name in zip(descriptor_sets, descriptor_names):
 		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=False)
-		best_model, best_score, best_hyperparameters = select_model(X_train, X_test, y_train, y_test, classification=classification)
+		best_model, best_score, best_hyperparameters = select_model(X_train, X_test, y_train, y_test, classification=classification, n_jobs=n_jobs)
 		print(names)
 		print("Best model: %s" % best_model)
 		print("Test score: %f" % best_score)
