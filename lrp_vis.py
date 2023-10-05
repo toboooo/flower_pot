a0944d014c2cf7377e015bcd2d7c034d605dc749
toboooo <60663629+toboooo@users.noreply.github.com>
2023-10-05 18:13:07 +0100

    Create lrp_vis.py

---
+++ lrp_vis.py
@@ -0,0 +1,2 @@
+# https://github.com/zzhang624/ML-SARS-CoV-2/blob/main/run_MLP_LRP_SHUFFLE_BOOTSTRAP_GITHUB.ipynb
+# https://www.cheminformania.com/learn-how-to-map-a-simple-ames-mutagenicity-model-to-molecular-features-using-rdkit/
